f579479a72a2e1526011c77962aa3f4306a9cdb4
Thiago Oliveira <thiago.oliveira@gft.com>
2017-07-24 17:59:25 -0300

    PARTIAL - Alterações para corrigir depois

--- population/src/App.js
+++ population/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Navbar, Nav, NavItem, Row, Col, FormControl, ControlLabel, Jumbotron, Grid } from 'react-bootstrap';
+import { 
+  Navbar, Nav, NavItem, Row, Col, Grid, FormGroup,
+  FormControl, ControlLabel, Jumbotron, 
+} from 'react-bootstrap';
 import { Bar } from 'react-chartjs';
 
 
@@ -8,18 +11,24 @@ class App extends Component {
 
 constructor(){
   super()
+  this.year = 1950;
   this.state = {
-    showHome: true,
-    showComparacao: false,
+    year: '',
+    age: '',
+    showHome: false,
+    showComparacao: true,
     showEstatistica: false,
-    chartData: { datasets: [{
+    validateAge : this.validateAge.bind(this),
+    validateYear: this.validateYear.bind(this),
+    chartData: { datasets: [
+        {
             data: [20, 50],
             label: 'Mulher',
             yAxisID: 'left-y-axis',
             fillColor : [
               'rgba(255, 99, 132, 0.2)'
             ]
-        }, {
+        },{
             data: [10, 40],
             label: 'Homem',
             yAxisID: 'right-y-axis',
@@ -38,6 +47,13 @@ constructor(){
         <Grid fluid={true}>
           { this.navbarInstance }
           { this.showView() }
+          <hr/>
+          <Row>
+            <Col>
+              <ControlLabel>País: Alemanha</ControlLabel>
+            </Col>
+          </Row>
+          
           <Bar data={this.state.chartData} width="400" height="250"/>
         </Grid>
       </div>
@@ -60,16 +76,23 @@ handleSelected(eventKey) {
 }
 
 validateYear(e){
-  const re = /[0-9A-F]+/g;
-    if (!re.test(e.key)) {
-      e.preventDefault();
+  const re = /^[a-z]{1950,2017}$/
+  
+  console.log(this)
+    if (!re.test(this.year)) {
+      return 'error';
+    } else {
+      return 'success'
     }
 }
 
 validateAge(e){
-  const re = /[0-9A-F]+/g;
-    if (!re.test(e.key)) {
-      e.preventDefault();
+  const re = /^[a-z]{0,100}$/
+  console.log(this.state)
+    if (!re.test(this.age)) {
+      return 'error';
+    } else {
+      return 'success';
     }
 }
 
@@ -89,32 +112,49 @@ navbarInstance = (
 
 homeInstance = (
   <Jumbotron>
-    <h1>População</h1>
-    <p>O intuito deste exemplo é fazer: </p>
-    <p>Uma comparação de uma população de um <strong>ano</strong> e uma <strong>idade</strong> com o ano atual</p>
-    <p>Uma progressão estatística do ano e idade escolhido com a análise 10 anos pra frente.</p>
-    <p>Navegue entre os menus na parte de cima</p>
+    <h1 className="text-left">População</h1>
+    <p className="text-left">O intuito deste exemplo é fazer: </p>
+    <hr/>
+    <ul >
+      <li className="text-left">Uma comparação de uma população de um <strong>ano</strong> e uma <strong>idade</strong> com o ano atual</li>
+      <li className="text-left">Uma progressão estatística do ano e idade escolhido com a análise 10 anos pra frente.</li>
+    </ul>
+    <hr/>
+    <p className="text-left">Navegue entre os menus na parte de cima</p>
   </Jumbotron>
 );
 
 comparacaoInstance = (
-  <Row>
-    <Col sm={4}>
-      <ControlLabel>Ano: </ControlLabel>
-      <FormControl type="number" placeholder="Ano" min="1950" max="2017" onKeyPress={(e) => this.validateYear(e).bind(this)}/>
-    </Col>
-    <Col sm={4}>
-      <ControlLabel>Idade: </ControlLabel>
-      <FormControl type="number" placeholder="Idade" min="0" max="100" onKeyPress={(e) => this.validateAge(e).bind(this)}/>
-    </Col>
-    <Col sm={4}>
-      <ControlLabel>País: </ControlLabel>
-      <FormControl componentClass="select" placeholder="select">
-        <option value="brasil">Brasil</option>
-        <option value="alemanha">Alemanha</option>
-      </FormControl>
-    </Col>
-  </Row>
+  <form>
+      <Row>
+        <Col sm={4}>
+        <FormGroup
+            controlId="formYear"
+            validationState={this.validateYear()} >
+          <ControlLabel>Ano: </ControlLabel>
+          <FormControl type="number" placeholder="Ano" value={this.year}/>
+          <FormControl.Feedback />
+        </FormGroup>
+        </Col>
+        <Col sm={4}>
+          <FormGroup
+              controlId="formAge"
+              validationState={this.validateAge()} >
+            <ControlLabel>Idade: </ControlLabel>
+            <FormControl type="number" placeholder="Idade" value={this.age}/>
+            <FormControl.Feedback />
+          </FormGroup>
+        </Col>
+        <Col sm={4}>
+          <ControlLabel>País: </ControlLabel>
+          <FormControl componentClass="select" placeholder="select">
+            <option value="brasil">Brasil</option>
+            <option value="alemanha">Alemanha</option>
+          </FormControl>
+        </Col>
+      </Row>
+      
+  </form>
 );
 
 showView() {
